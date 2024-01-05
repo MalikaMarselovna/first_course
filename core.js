58fcc2ec8f0395a5023e2656ce760172c1423b51
MalikaMarselovna <mmukashova1@gmail.com>
2024-01-05 14:07:29 +0600

    add 2-section .order-course

---
+++ core.js
@@ -0,0 +1,29 @@
+const days = document.querySelector('#days');
+const hours = document.querySelector('#hours');
+const minutes = document.querySelector('#minutes');
+const seconds = document.querySelector('#seconds');
+
+const currentYear = new Date().getFullYear();
+const nextYear = new Date(`March 03 2024 00:00:00`);
+
+
+function updateCounter() {
+    const currentTime = new Date();
+    const diff = nextYear - currentTime;
+
+    const daysLeft = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const hoursLeft = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const minutesLeft = Math.floor(diff / 1000 / 60) % 60;
+    const secondsLeft = Math.floor(diff / 1000) % 60;
+
+
+    days.innerText = daysLeft;
+    hours.innerText = hoursLeft < 10 ? '0' + hoursLeft : hoursLeft;
+    minutes.innerText = minutesLeft < 10 ? '0' + minutesLeft : minutesLeft;
+    seconds.innerText = secondsLeft < 10 ? '0' + secondsLeft : secondsLeft;
+}
+
+updateCounter();
+
+setInterval(updateCounter, 1000);
+
